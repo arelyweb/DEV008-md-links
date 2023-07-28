724b55cae6882b9592febdcb21ed98ac814ecb6f
Arely Gonzalez <arely.web@gmail.com>
2023-07-28 13:07:14 -0600

    validar ruta de inicio

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
+const main = require('./lib/main');
+import msg from 'chalk';
+
+//obtenemos el path
+const argv = process.argv[2];
+
+
+if(main.validarPath(argv)){//validamos la ruta 
+   console.log(msg.green('Ruta Válida'));
+}else{
+    console.log(msg.red('Ruta Inválida'));
 };
+
+
+
+
+
